5e274337b14a08bf7503145b39cee80a184acfd7
Chris Peabody <cgpzbd@mst.edu>
2019-08-25 17:26:01 -0500

    Set up initial bare bones for getting started.

---
+++ CommunityJam2019/Assets/Scripts/Beat.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Beat {
+	protected string _words;
+
+	public Beat (string words) {
+		_words = words;
+	}
+
+	public string getWords() {return _words;}
+	public void setWords(string words) {_words = words;}
+}
+
+public class Choice : Beat {
+	private Page _link;
+
+	public Choice (string words, Page link) : base(words) {
+		_link = link;
+	}
+
+	public Page getLink() {return _link;}
+	public void setLink(Page link) {_link = link;}
+}
---
+++ CommunityJam2019/Assets/Scripts/Page.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Page : ScriptableObject {
+	List<Beat> _beats;
+	List<Choice> _choices;
+}
