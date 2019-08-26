3b0729d4848e5057c89ebd2d68577aba2a995e0e
Chris Peabody <cgpzbd@mst.edu>
2019-08-25 21:59:38 -0500

    Created a primative asset editor for our 'Pages'. Could benefit from more work though.

---
+++ CommunityJam2019/Assets/Editor/PageEditor.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Page))]
+public class PageEditor : Editor {
+
+    public override void OnInspectorGUI() {
+    	//base.OnInspectorGUI();
+
+    	serializedObject.Update();
+    	Page page = (Page) target;
+
+    	Beat beatToRemove = null;
+    	foreach(Beat beat in page.beats) {
+    		GUILayout.BeginHorizontal();
+
+    		if (GUILayout.Button("X", GUILayout.Width(18))) {
+    			beatToRemove = beat;
+    		}
+
+    		beat.setWords(EditorGUILayout.TextArea(beat.getWords()));
+    		GUILayout.EndHorizontal();
+    	}
+    	if (beatToRemove != null) {page.beats.Remove(beatToRemove);}
+
+    	if (GUILayout.Button("Add beat")) {
+    		page.beats.Add(new Beat(""));
+    	}
+
+		Choice choiceToRemove = null;
+    	foreach(Choice choice in page.choices) {
+    		GUILayout.BeginHorizontal();
+
+    		if (GUILayout.Button("X", GUILayout.Width(18))) {
+    			choiceToRemove = choice;
+    		}
+
+    		choice.setWords(EditorGUILayout.TextField("Choice:", choice.getWords()));
+    		GUILayout.EndHorizontal();
+
+    		choice.setLink((Page) EditorGUILayout.ObjectField("Page link:", choice.getLink(), typeof(Page), false));
+    	}
+    	if (choiceToRemove != null) {page.choices.Remove(choiceToRemove);}
+
+    	if (page.choices.Count < 4) {
+	    	if (GUILayout.Button("Add choice")) {
+	    		page.choices.Add(new Choice("", null));
+	    	}
+    	}
+
+    	serializedObject.ApplyModifiedProperties();
+    	EditorUtility.SetDirty(page);
+    }
+}
--- CommunityJam2019/Assets/Scripts/Page.cs
+++ CommunityJam2019/Assets/Scripts/Page.cs
@@ -4,6 +4,6 @@ using UnityEngine;
 
 [CreateAssetMenu(menuName = "Page")]
 public class Page : ScriptableObject {
-	[SerializeField] public List<Beat> beats;
-	[SerializeField] public List<Choice> choices;
+	[SerializeField] public List<Beat> beats = new List<Beat>();
+	[SerializeField] public List<Choice> choices = new List<Choice>();
 }
